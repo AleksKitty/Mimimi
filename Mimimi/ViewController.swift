f327feb0225995c79b115030901a97630c987552
Aleksandra <alexandra.tochansky@gmail.com>
2020-03-01 00:33:39 +0300

    Initial Commit

---
+++ Mimimi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mimimi
+//
+//  Created by Aleksandra Tochanskaia on 01.03.2020.
+//  Copyright © 2020 BigKittens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
